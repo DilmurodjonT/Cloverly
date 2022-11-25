18b3e244e49c335f828221b1ece1222c601ee89f
DilmurodjonT <dilmurod_tadjibaev@mail.ru>
2022-11-25 22:56:41 +0500

    first commit

---
+++ script/script.js
@@ -0,0 +1,41 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      container: {
+        center: true,
+      },
+      colors: {
+        "bg-color-D8A": "#1FBD8A",
+        "bg-color-BF9": "#F4FBF9",
+        "bg-color-ab2": "#3aeab2",
+        "bg-color-F44": "#192F44",
+        "bg-color-CF5": "#FBFCF5",
+        "bg-color-4E8": "#F2F4E8",
+        "bg-color-540": "#0A2540",
+        "bg-color-844": "#193844",
+        "color-75B": "#24475B",
+        "color-A90": "#00CA90",
+        "color-A51": "#013A51",
+        "color-782": "#5C7782",
+      },
+      FontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+      },
+      spacing: {
+        x19: "19px",
+      },
+      boxShadow: {
+        headerShadow: "0px 5.69882px 9.49804px rgba(19, 40, 29, 0.098)",
+      },
+      maxWidth: {
+        maxWidth846: "846px",
+        maxWidth518: "518px",
+        maxWidth250: "250px",
+      },
+      fontSize: {
+        size21: "21px",
+        FWeight: "28px",
+      },
+    },
+  },
+};
